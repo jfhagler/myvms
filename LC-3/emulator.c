13b1c38ad3f97369fefe16ca7abf355266bd6105
Jacob <jacobfhagler@gmail.com>
2024-06-25 06:54:16 -0400

    somewhat function

--- LC-3/emulator.c
+++ LC-3/emulator.c
@@ -2,11 +2,18 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/termios.h>
+#include <sys/mman.h>
+
 
 #define MEMORY_MAX (1<<16)
-#define PC_START 0x3000
 
-bool running = 1; 
+int running; 
 uint16_t memory[MEMORY_MAX];
 
 enum {
@@ -72,12 +79,6 @@ uint16_t sign_extend(uint16_t x, int bit_count){
 	return x;
 }
 
-int mem_read() {
-}
-
-int mem_write() {
-}
-
 int update_flags(uint16_t r) {
 	if (reg[r] == 0) {
 		reg[R_COND] = FL_ZERO;
@@ -93,7 +94,7 @@ uint16_t swap16(uint16_t x) {
 }
 
 void read_image_file(FILE* file){
-	uint16_t origin
+	uint16_t origin;
 	fread(&origin, sizeof(origin), 1, file);
 	origin = swap16(origin); 
 	uint16_t max_read = MEMORY_MAX - origin;
@@ -117,6 +118,29 @@ void mem_write(uint16_t address, uint16_t val) {
 	memory[address] = val;
 }
 
+struct termios original_tio; 
+
+void disable_input_buffering() {
+ 	tcgetattr(STDIN_FILENO, &original_tio);
+	struct termios new_tio = original_tio;
+	new_tio.c_lflag &= ~ICANON & ~ECHO;
+	tcsetattr(STDIN_FILENO, TCSANOW, &new_tio); 
+}
+
+void restore_input_buffering() {
+	tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
+}
+
+uint16_t check_key() {
+	fd_set readfds;
+	FD_ZERO(&readfds);
+	FD_SET(STDIN_FILENO, &readfds);
+	struct timeval timeout;
+	timeout.tv_sec = 0;
+	timeout.tv_usec = 0; 
+	return select(1, &readfds, NULL, NULL, &timeout) !=0;
+}
+
 uint16_t mem_read(uint16_t address) {
 	if (address == MR_KBSR) {
 		if (check_key()) {
@@ -129,7 +153,7 @@ uint16_t mem_read(uint16_t address) {
 	return memory[address]; 
 }
 
-int fetch_eval_execute() { 
+int fetch_eval_execute() {
 	uint16_t instr = mem_read(reg[R_PC]++);
 	uint16_t op = instr >> 12;
 	switch (op) {
@@ -207,7 +231,7 @@ int fetch_eval_execute() {
 			update_flags(r0);
 			break;
 		}
-		case OP_LDR:{
+		case OP_LDR: {
 			uint16_t dr = (instr >> 9) & 0x7;
 			uint16_t br = (instr >> 6) & 0x7; 
 			uint16_t offset = sign_extend( instr & 0x3F, 6);
@@ -215,46 +239,46 @@ int fetch_eval_execute() {
 			update_flags(dr);
 			break;
 		}
-		case OP_LEA:{
+		case OP_LEA: {
 			uint16_t dr = (instr >> 9) & 0x7;
 			uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
 			reg[dr] = reg[R_PC] + pc_offset;
 			update_flags(dr);  
 			break;
 		}
-		case OP_ST:{
+		case OP_ST: {
 			uint16_t sr = (instr >> 9) & 0x7;
 			uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
-			mem_write(reg[R_PC] + pc_offset); 
+			mem_write(reg[R_PC] + pc_offset, reg[sr]); 
 			break;
 		}
-		case OP_STI:{
+		case OP_STI: {
 			uint16_t sr = (instr >> 9) & 0x7;
 			uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
 			mem_write(mem_read(reg[R_PC] + pc_offset), reg[sr]); 
 			break;
 		}
-		case OP_STR:{
+		case OP_STR: {
 			uint16_t sr = (instr >> 9) & 0x7;
 			uint16_t br = (instr >> 9) & 0x7;
 			uint16_t pc_offset = sign_extend(instr & 0x3F, 6); 
 			mem_write(reg[br] + pc_offset, reg[sr]);
 			break;
 		}
-		case OP_TRAP:{
+		case OP_TRAP: {
 			reg[R_R7] = reg[R_PC];
 			switch (instr & 0xFF) { 
-				case TRAP_GETC:{
+				case TRAP_GETC: {
 					reg[R_R0] = (uint16_t)getchar();
 					update_flags(R_R0);
 					break;
 				}
-				case TRAP_OUT:{
+				case TRAP_OUT: {
 					putc((char)reg[R_R0], stdout);
 					fflush(stdout);
 					break;
 				}
-				case TRAP_PUTS:{
+				case TRAP_PUTS: {
 					uint16_t *c = memory + reg[R_R0];
 					while (*c) {
 						putc((char)*c , stdout);
@@ -263,7 +287,7 @@ int fetch_eval_execute() {
 					fflush(stdout); 
 					break;
 				}
-				case TRAP_IN:{
+				case TRAP_IN: {
 					printf("Enter a character: ");
 					char c = getchar();
 					putc(c, stdout);
@@ -272,7 +296,7 @@ int fetch_eval_execute() {
 					update_flags(R_R0);
 					break;
 				}
-				case TRAP_PUTSP:{
+				case TRAP_PUTSP: {
 					uint16_t *c = memory + reg[R_R0];
 					while (*c) {
 				 		char char1 = (*c) & 0xFF;
@@ -284,7 +308,7 @@ int fetch_eval_execute() {
 					fflush(stdout); 	
 					break;
 				}
-				case TRAP_HALT:{
+				case TRAP_HALT: {
 					puts("HALT");
 					fflush(stdout);
 					running = 0; 
@@ -302,17 +326,42 @@ int fetch_eval_execute() {
 	} 
 }
 
+void handle_interrupt(int signal) {
+	restore_input_buffering();
+	printf("\n");
+	exit(-2);
+}
+
 int main(int argc, const char **argv) {
-	if (argv < 2) {	
+	signal(SIGINT, handle_interrupt);
+	disable_input_buffering(); 	
+	
+	if (argc < 2) {	
 		printf("lc3 [image-file1] .. \n");
 		exit(2);
 	}
 
 
-	for (int j = 1; j < argc; j++) {
+	for (int j = 1; j < argc; ++j) {
 		if (!read_image(argv[j])) {
 			printf("failed to load image: %s\n", argv[j]);
 			exit(1);	
 		}
 	}
+
+	reg[R_COND] = FL_ZERO;
+	enum {PC_START = 0x3000};
+	reg[R_PC] = PC_START; 
+	running = 1; 
+	
+	while(running) {
+		fetch_eval_execute();	
+	}
+	restore_input_buffering();
+
+
+
+
+
+
 }
